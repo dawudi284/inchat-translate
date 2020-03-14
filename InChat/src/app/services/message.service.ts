f431495d5c3a8a1b58c2eecaedf888fa457aa871
Joe Ciccone <ciccone.n.joseph@gmail.com>
2020-03-14 18:10:33 -0400

    implemented delteUser() differently, added it to home

--- InChat/src/app/services/message.service.ts
+++ InChat/src/app/services/message.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { User } from 'firebase';
+import { AuthService } from './auth.service';
+import { Message, TranslationEntity} from '../models/message.model';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
   chatId: string;
-  constructor(private db: AngularFirestore) { }
+  constructor(private db: AngularFirestore,  private afAuth: AuthService) { }
 
-  getMessages() {
-    return this.db.collection('chats/' + this.chatId + '/messages', ref => ref.orderBy('sendTime')).valueChanges();
+  sendMessages(messageContent: string) {
+    let message: Message;
+    message.messId = '123';
+    message.chatId = '456';
+    let translation: TranslationEntity = {
+      language: 'english',
+      message: 'hello'
+    };
+    message.translation.push(translation);
+    this.db.collection('messages').add(message);
   }
 
 }
