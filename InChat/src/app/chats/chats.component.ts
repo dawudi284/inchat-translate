da638fbf68221ad9b56ca79b2550e581195fa06f
Joe Ciccone <ciccone.n.joseph@gmail.com>
2020-03-26 22:47:48 -0400

    big commit, pretty much have messages working. More work to do

--- InChat/src/app/chats/chats.component.ts
+++ InChat/src/app/chats/chats.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../services/chat.service';
 import { AuthService } from '../services/auth.service';
-import { auth } from 'firebase/app';
 import { MessageService } from '../services/message.service';
+import { Message } from '../models/message.model';
 import { UserService } from '../services/user.service';
+import { SettingsService } from '../services/settings.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-chats',
@@ -18,15 +20,36 @@ export class ChatsComponent implements OnInit {
     public messageService: MessageService,
     private userService: UserService) { }
 
+  messages;
+  translatedMessages: string[] = [];
+
   ngOnInit() {
     this.afAuth.Auth.auth.onAuthStateChanged(() => {
       if (this.afAuth.Auth.auth.currentUser === null) {
         console.log('No chats to display');
-        return;
       } else {
         console.log(this.afAuth.Auth.auth.currentUser.uid);
-        return;
+        if (this.userService.getCurrentUser() === undefined) {
+          this.userService.getUser(this.afAuth.Auth.auth.currentUser.uid).subscribe(user => {
+            this.userService.setCurrentUser(user as User);
+            this.messageService.getMessages('test-id').subscribe(response => {
+              response.sort(this.messageSorter);
+              this.messages = response;
+            });
+          });
+        }
       }
     });
+
+
   }
+  messageSorter(a: Message, b: Message) {
+    if (a.timeSent === b.timeSent) {
+      return 0;
+    } else {
+      return a.timeSent < b.timeSent ? -1 : 1;
+    }
+  }
+
 }
+
